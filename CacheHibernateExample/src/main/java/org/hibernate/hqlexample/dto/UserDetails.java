4b45c45e3ae7d92aefc993b5d5fed60935117366
Ram Krishna Maharjan <ramkrmaharjan@hotmail.com>
2014-10-17 19:40:36 +0545

    Cache Hibernate Copy[Don't refer , refer next commit]

---
+++ CacheHibernateExample/src/main/java/org/hibernate/hqlexample/dto/UserDetails.java
@@ -0,0 +1,41 @@
+package org.hibernate.hqlexample.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+// (name="USER_TABLE")
+@Table(name = "USER_TABLE")
+@NamedQuery(name="findUserDetails",query="from UserDetails where userId =?")
+public class UserDetails {
+
+	@Id
+	@GeneratedValue
+	private int userId;
+
+	@Column(name = "USER_NAME")
+	private String userName;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	
+
+}
---
+++ CacheHibernateExample/src/main/java/org/hibernate/hqlexample/main/HQLMain.java
@@ -0,0 +1,57 @@
+package org.hibernate.hqlexample.main;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
+import org.hibernate.hqlexample.dto.UserDetails;
+
+public class HQLMain {
+
+	public static void main(String[] args) {
+		Configuration c = new Configuration().configure();
+		SessionFactory sessionFactory = c.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		session.beginTransaction();
+		
+//		Query query = session.createQuery("from UserDetails where userId = ?");
+//		query.setInteger(0, 2);
+		
+//		Query query = session.createQuery("from UserDetails where userId = :userID");
+//		query.setInteger("userID", 2);
+//		query.setMaxResults(5);
+//		query.setFirstResult(0);
+		
+//		Query query = session.getNamedQuery("findUserDetails");
+//		query.setInteger(0, 2);
+//		List<UserDetails> list = query.list();
+		
+//		Criteria criteria = session.createCriteria(UserDetails.class);
+		//criteria.add(Restrictions.eq("userId", 2)).add(Restrictions.eq("userName","Ramesh"));
+		//criteria.add(Restrictions.or(Restrictions.eq("userId", 2),Restrictions.eq("userId", 2)));
+		
+		
+		//Example use
+		
+		UserDetails exampleUser = new UserDetails();
+	//	exampleUser.setUserId(2); //Example doesn't consider Primary ID
+		exampleUser.setUserName("Ramesh");
+		Example userDetailsExample = Example.create(exampleUser).excludeProperty("userName");
+		
+		//Projection and query by example
+		Criteria criteria = session.createCriteria(UserDetails.class)
+						.add(userDetailsExample);
+		//Selects username only
+		List<UserDetails> list = (List<UserDetails>)criteria.list();
+		for(UserDetails d : list) {
+			System.out.println(d.getUserId());
+		}
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
