b446313d70f956aed934cd1c2566ab82732809aa
Ram Krishna Maharjan <ramkrmaharjan@hotmail.com>
2014-10-17 19:28:16 +0545

    Query By Example(Hibernate)

--- HQL/src/main/java/org/hibernate/hqlexample/main/HQLMain.java
+++ HQL/src/main/java/org/hibernate/hqlexample/main/HQLMain.java
@@ -6,9 +6,8 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.hibernate.hqlexample.dto.UserDetails;
 
 public class HQLMain {
@@ -36,9 +35,18 @@ public class HQLMain {
 		//criteria.add(Restrictions.eq("userId", 2)).add(Restrictions.eq("userName","Ramesh"));
 		//criteria.add(Restrictions.or(Restrictions.eq("userId", 2),Restrictions.eq("userId", 2)));
 		
+		
+		//Example use
+		
+		UserDetails exampleUser = new UserDetails();
+	//	exampleUser.setUserId(2); //Example doesn't consider Primary ID
+		exampleUser.setUserName("Ramesh");
+		Example userDetailsExample = Example.create(exampleUser);
+		
 		//Projection and query by example
 		Criteria criteria = session.createCriteria(UserDetails.class)
-				.setProjection(Projections.property("userName"));
+				.setProjection(Projections.property("userName"))
+				.add(userDetailsExample);
 		//Selects username only
 		List<String> list = (List<String>)criteria.list();
 		for(String d : list) {
