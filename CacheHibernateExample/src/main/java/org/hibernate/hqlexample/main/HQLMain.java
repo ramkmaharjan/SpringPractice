ab04fe71bdec5d2783118ae523180eda20438eb7
Ram Krishna Maharjan <ramkrmaharjan@hotmail.com>
2014-10-17 20:05:53 +0545

    1st level cache implemented

--- CacheHibernateExample/src/main/java/org/hibernate/hqlexample/main/HQLMain.java
+++ CacheHibernateExample/src/main/java/org/hibernate/hqlexample/main/HQLMain.java
@@ -17,39 +17,15 @@ public class HQLMain {
 		Session session = sessionFactory.openSession();
 
 		session.beginTransaction();
+	
+		//1st Search query
+		UserDetails user1 = (UserDetails) session.get(UserDetails.class,2);
 		
-//		Query query = session.createQuery("from UserDetails where userId = ?");
-//		query.setInteger(0, 2);
-		
-//		Query query = session.createQuery("from UserDetails where userId = :userID");
-//		query.setInteger("userID", 2);
-//		query.setMaxResults(5);
-//		query.setFirstResult(0);
-		
-//		Query query = session.getNamedQuery("findUserDetails");
-//		query.setInteger(0, 2);
-//		List<UserDetails> list = query.list();
-		
-//		Criteria criteria = session.createCriteria(UserDetails.class);
-		//criteria.add(Restrictions.eq("userId", 2)).add(Restrictions.eq("userName","Ramesh"));
-		//criteria.add(Restrictions.or(Restrictions.eq("userId", 2),Restrictions.eq("userId", 2)));
-		
-		
-		//Example use
-		
-		UserDetails exampleUser = new UserDetails();
-	//	exampleUser.setUserId(2); //Example doesn't consider Primary ID
-		exampleUser.setUserName("Ramesh");
-		Example userDetailsExample = Example.create(exampleUser).excludeProperty("userName");
-		
-		//Projection and query by example
-		Criteria criteria = session.createCriteria(UserDetails.class)
-						.add(userDetailsExample);
-		//Selects username only
-		List<UserDetails> list = (List<UserDetails>)criteria.list();
-		for(UserDetails d : list) {
-			System.out.println(d.getUserId());
-		}
+		//2nd Search query
+		//This one is not called because of one level cache.
+		UserDetails user2 = (UserDetails) session.get(UserDetails.class,2);
+		user2.setUserName("Lali");
+		System.out.println(user2.getUserName());
 		session.getTransaction().commit();
 		session.close();
 		sessionFactory.close();
