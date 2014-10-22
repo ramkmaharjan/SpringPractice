bba744ed5c587d2a4618d92902197428a36f8775
Ram Krishna Maharjan <ramkrmaharjan@hotmail.com>
2014-10-22 13:29:26 +0545

    @PathVariables use of Map

--- FirstSpringMVCExample/src/main/java/com/mvcexample/myapp/PathVariableController.java
+++ FirstSpringMVCExample/src/main/java/com/mvcexample/myapp/PathVariableController.java
@@ -1,5 +1,7 @@
 package com.mvcexample.myapp;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,17 +11,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/path")
 public class PathVariableController {
 
-	@RequestMapping("/hello/{name}")
-	public String sayHelloName(@PathVariable("name") String name,Model model) {
-		
-		model.addAttribute("helloMsg","Hello"+name);
-		return "hello";
-	}
-	
-	@RequestMapping("/hello/{name}/{address}")
-	public String sayHelloNameAndAddress(@PathVariable("name") String name,@PathVariable("address") String address,Model model) {
-		
-		model.addAttribute("helloMsg","Hello:"+name+":Welcome from:"+address);
+	/*
+	 * @RequestMapping("/hello/{name}") public String
+	 * sayHelloName(@PathVariable("name") String name,Model model) {
+	 * 
+	 * model.addAttribute("helloMsg","Hello"+name); return "hello"; }
+	 * 
+	 * @RequestMapping("/hello/{name}/{address}") public String
+	 * sayHelloNameAndAddress(@PathVariable("name") String
+	 * name,@PathVariable("address") String address,Model model) {
+	 * 
+	 * model.addAttribute("helloMsg","Hello:"+name+":Welcome from:"+address);
+	 * return "hello"; }
+	 */
+
+	@RequestMapping("/hello/{name}/{address}/{phoneno}")
+	public String sayHelloAll(@PathVariable Map<String, String> pathVal,
+			Model model) {
+		String name = pathVal.get("name");
+		String address = pathVal.get("address");
+		String phnumber = pathVal.get("phoneno");
+		model.addAttribute("helloMsg", "Hello:" + name + ":Welcome from:"
+				+ address + ": I will call you at " + phnumber);
 		return "hello";
 	}
 }
