64e07051fcc6f18f27d95269ce0e7799cba4134b
Ram Krishna Maharjan <ramkrmaharjan@hotmail.com>
2014-10-22 17:32:52 +0545

    Model Attribute use..

--- FirstSpringMVCExample/src/main/java/com/mvcexample/myapp/UserFormController.java
+++ FirstSpringMVCExample/src/main/java/com/mvcexample/myapp/UserFormController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,12 +33,10 @@ public class UserFormController {
 	
 	@RequestMapping(value="/submitForm", method =RequestMethod.POST)
 	//....@RequestParam Map<String,String> paramList //we can do like this also
-	public String submitForm(@RequestParam("studentName") String name, @RequestParam("studentAddress") String address,Model model) {
-		Student student = new Student();
-		student.setStudentName(name);
-		student.setStudentAddress(address);
-		model.addAttribute("submitMsg","You submitted:name:"+ name+":Address:" + address);
-		model.addAttribute("student1",student);
+	public String submitForm(@ModelAttribute("student1") Student student,Model model) {
+		
+		model.addAttribute("submitMsg","You submitted:name:"+ student.getStudentName()+":Address:" + student.getStudentAddress());
+		
 		return "submitform";
 	}
 }
